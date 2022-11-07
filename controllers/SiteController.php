f6a04e933a6007a170596400ed52555f8342c2f7
SilverHayasaka <silver.zh@hotmail.com>
2022-11-07 21:23:47 +0800

    yii图片上传

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -129,21 +129,4 @@ class SiteController extends Controller
         return $this->render('about');
     }
 
-
-
-    public function actionSay($message = 'Hello')
-    {
-        return $this->render('say', ['message' => $message]);
-    }
-
-    public function actionEntry()
-    {
-        $model = new EntryForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            return $this->render('entry-confirm', ['model' => $model]);
-        } else {
-            return $this->render('entry', ['model' => $model]);
-        }
-    }
-
 }
--- views/message/upload.php
+++ views/message/upload.php
@@ -4,5 +4,6 @@ use yii\widgets\ActiveForm; ?>
 
 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
 <?= $form->field($model, 'file')->fileInput() ?>
+    <br/>
     <button>Submit</button>
 <?php ActiveForm::end(); ?>
